83ff1dd9235dc78dea706f5dcda0be5f6cd6c77e
chuanqiong <rubyvirusqq@gmail.com>
2017-05-14 15:40:20 +0800

    add help url.

--- src/main/java/com/whatistest/composite/AbstractFile.java
+++ src/main/java/com/whatistest/composite/AbstractFile.java
@@ -2,6 +2,7 @@ package com.whatistest.composite;
 
 /**
  * Created by rubyvirusqq@gmail.com on 14/05/17.
+ * https://gof.quanke.name/%E6%A0%91%E5%BD%A2%E7%BB%93%E6%9E%84%E7%9A%84%E5%A4%84%E7%90%86%E2%80%94%E2%80%94%E7%BB%84%E5%90%88%E6%A8%A1%E5%BC%8F%EF%BC%88%E4%BA%8C%EF%BC%89.html
  */
 abstract class AbstractFile {
 
